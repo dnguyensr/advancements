a28a4e1c203df36b089a50f8440649a6b9224d7a
dnguyensr <danielnguyensr@gmail.com>
2017-02-14 17:22:49 -0800

    Add users controller and views, disabled in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       end
     end
   end
+  # get 'users' => 'users#index'
+  # get 'users/:id' => 'users#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
