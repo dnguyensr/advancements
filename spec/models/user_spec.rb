76a266c23c5afb7bcb28c44c2cfb6e210f293561
dnguyensr <danielnguyensr@gmail.com>
2017-02-24 01:29:48 -0800

    Add test for user class method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:email) }
+
+  it "has a name consisting of first name and first initial of last name" do
+    user = User.new(first_name: "Boy", last_name: "Scout")
+    expect(user.name).to eq("Boy S")
+  end
 end
