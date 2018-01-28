a3139905a9b0a0c3a136e0f3893049d3db487d5c
dnguyensr <danielnguyensr@gmail.com>
2018-01-27 18:38:09 -0800

    Update routes, replace binstubs, add procfiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "home#index"
   resources :advancements do
     resources :ranks do
       resources :requirements
@@ -7,6 +9,4 @@ Rails.application.routes.draw do
   end
   # get 'users' => 'users#index'
   # get 'users/:id' => 'users#show'
-  devise_for :users
-root to: "home#index"
 end
